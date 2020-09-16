c7d29d3a45d86202d18d7c62d3d9a97880d0e201
Simon Andersen <simonandersen@Simons-MacBook-Air-2.local>
2020-09-16 10:16:59 +0200

    city html + js to create and populate header with city name + background image

--- city.js
+++ city.js
@@ -4,26 +4,32 @@ let cityFromUrl = params.get("city");
 
 fetch("https://spreadsheets.google.com/feeds/list/18QShemZlLoq2j6zasY3bNjFkoqkDb_tP7Tpjujma3mg/od6/public/values?alt=json")
     .then(res => res.json())
-    .then(showCities)
+    .then(showActivities)
 
-function showCities(data) {
+
+function showActivities(data) {
     console.log(data)
     data.feed.entry.forEach(city => {
+        // CHECKING IF CITYNAME IN DATA MATCHES CITY IN URL
         if (city.gsx$city.$t == cityFromUrl) {
+
+            // SET HEADER H1 TO CITY NAME
+            document.querySelector(".city-name").textContent = city.gsx$city.$t;
+            // SET HEADER BACKGROUND IMAGE TO CITY HERO IMAGE
+            document.querySelector("header").setAttribute("style", "background-image: url(http://ssays.dk/kea/common_interest_images/" + city.gsx$heroimage.$t + ".jpg);")
+
+            // CREATE SINGLE ACTIVTY TEMPLATE
             const template = document.querySelector("template").content;
             const copy = template.cloneNode(true);
-            copy.querySelector("h1").textContent = city.gsx$city.$t;
+            //copy.querySelector("h1").textContent = city.gsx$city.$t;
 
-            //CITY NAME
-            copy.querySelector("a").href = `city.html?city=${city.gsx$city.$t}`
-
-            //POPULATE TEMPLATE WITH DATA
+            // POPULATE TEMPLATE WITH DATA
             copy.querySelector('h2').textContent = city.gsx$activitytype.$t;
             copy.querySelector('h3').textContent = city.gsx$venuename.$t;
             copy.querySelector('.headline').textContent = city.gsx$headline.$t;
             copy.querySelector("img").setAttribute("src", "http://ssays.dk/kea/common_interest_images/" + city.gsx$image.$t + ".jpg");
 
-            //APPEND
+            // APPEND TEMPLATE TO MAIN
             document.querySelector("main").appendChild(copy);
         }
     })
