7bc2d8ba49b912a8e6f537d933afa7359b9aa081
Simon Andersen <simonandersen@Simons-MacBook-Air-2.local>
2020-09-16 09:33:26 +0200

    Initial commit

--- city.js
+++ city.js
@@ -2,31 +2,30 @@ let params = new URLSearchParams(document.location.search.substring(1));
 let cityFromUrl = params.get("city");
 
 
-fetch(
-    "https://spreadsheets.google.com/feeds/list/18QShemZlLoq2j6zasY3bNjFkoqkDb_tP7Tpjujma3mg/od6/public/values?alt=json"
-  ).then(res => res.json())
-  .then(showCities)
+fetch("https://spreadsheets.google.com/feeds/list/18QShemZlLoq2j6zasY3bNjFkoqkDb_tP7Tpjujma3mg/od6/public/values?alt=json")
+    .then(res => res.json())
+    .then(showCities)
 
 function showCities(data) {
-  console.log(data)
-  data.feed.entry.forEach(city => {
-    if (city.gsx$city.$t == cityFromUrl) {
-      const template = document.querySelector("template").content;
-      const copy = template.cloneNode(true);
-      copy.querySelector("h1").textContent = city.gsx$city.$t;
+    console.log(data)
+    data.feed.entry.forEach(city => {
+        if (city.gsx$city.$t == cityFromUrl) {
+            const template = document.querySelector("template").content;
+            const copy = template.cloneNode(true);
+            copy.querySelector("h1").textContent = city.gsx$city.$t;
 
-      //CITY NAME
-      copy.querySelector("a").href = `city.html?city=${city.gsx$city.$t}`
+            //CITY NAME
+            copy.querySelector("a").href = `city.html?city=${city.gsx$city.$t}`
 
-      //POPULATE TEMPLATE WITH DATA
-       copy.querySelector('h2').textContent = city.gsx$activitytype.$t;
-      copy.querySelector('h3').textContent = city.gsx$venuename.$t;
-      copy.querySelector('.headline').textContent = city.gsx$headline.$t;
-       copy.querySelector("img").setAttribute("src", "http://ssays.dk/kea/common_interest_images/" + city.gsx$image.$t + ".jpg"); 
-     
-      //APPEND
-      document.querySelector("main").appendChild(copy);
-    }
-  })
+            //POPULATE TEMPLATE WITH DATA
+            copy.querySelector('h2').textContent = city.gsx$activitytype.$t;
+            copy.querySelector('h3').textContent = city.gsx$venuename.$t;
+            copy.querySelector('.headline').textContent = city.gsx$headline.$t;
+            copy.querySelector("img").setAttribute("src", "http://ssays.dk/kea/common_interest_images/" + city.gsx$image.$t + ".jpg");
+
+            //APPEND
+            document.querySelector("main").appendChild(copy);
+        }
+    })
 
 }
